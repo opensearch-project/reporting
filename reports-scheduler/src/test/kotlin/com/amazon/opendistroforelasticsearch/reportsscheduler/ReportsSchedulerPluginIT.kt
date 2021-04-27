d420a54911fc94c7b5070c33b0a08bc36d41c9d1
Zhongnan Su <szhongna@amazon.com>
2021-04-26 21:02:28 -0700

    [Migration] Build reporting against OpenSearch 1.0.0-alpha2 (#25)

--- reports-scheduler/src/test/kotlin/com/amazon/opendistroforelasticsearch/reportsscheduler/ReportsSchedulerPluginIT.kt
+++ reports-scheduler/src/test/kotlin/com/amazon/opendistroforelasticsearch/reportsscheduler/ReportsSchedulerPluginIT.kt
@@ -43,7 +43,7 @@ class ReportsSchedulerPluginIT : OpenSearchIntegTestCase() {
         val pluginInfos = nodesInfoResponse.nodes[0].getInfo(PluginsAndModules::class.java).pluginInfos
         assertTrue(
             pluginInfos.stream()
-                .anyMatch { pluginInfo: PluginInfo -> pluginInfo.name == "opendistro-job-scheduler" })
+                .anyMatch { pluginInfo: PluginInfo -> pluginInfo.name == "opensearch-job-scheduler" })
         assertTrue(
             pluginInfos.stream()
                 .anyMatch { pluginInfo: PluginInfo -> pluginInfo.name == "opensearch-reports-scheduler" })
