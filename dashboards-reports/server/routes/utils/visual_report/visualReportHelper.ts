7e76fc6cff63360f27bc6f5c00fea0ac498eaff2
Gert Keldermans <gert.keldermans@global.ntt>
2021-06-17 09:25:42 +0200

    remove exit full screen button

--- dashboards-reports/server/routes/utils/visual_report/visualReportHelper.ts
+++ dashboards-reports/server/routes/utils/visual_report/visualReportHelper.ts
@@ -134,6 +134,10 @@ export const createVisualReport = async (
       document
         .querySelectorAll("[class^='euiHeader']")
         .forEach((e) => e.remove());
+      // remove exit full screen button
+      document
+        .querySelectorAll("[class^='dshExitFullScreenButton']")
+        .forEach(e => e.remove())
       // remove visualization editor
       if (reportSource === REPORT_TYPE.visualization) {
         document
