54a17b0ee0f62a75e7a0e5bc9a25ea096f9790a7
David Cui <davidcui@amazon.com>
2021-07-09 19:49:26 -0700

    add default timeout

--- dashboards-reports/.cypress/integration/01-create.spec.ts
+++ dashboards-reports/.cypress/integration/01-create.spec.ts
@@ -28,11 +28,8 @@ describe('Adding sample data', () => {
   it('Adds sample data', () => {
     cy.visit(`${Cypress.env('opensearchDashboards')}/app/home#/tutorial_directory/sampleData`);
     cy.get('div[data-test-subj="sampleDataSetCardflights"]').contains(/(Add|View) data/).click();
-    cy.wait(3000);
     cy.get('div[data-test-subj="sampleDataSetCardecommerce"]').contains(/(Add|View) data/).click();
-    cy.wait(3000);
     cy.get('div[data-test-subj="sampleDataSetCardlogs"]').contains(/(Add|View) data/).click();
-    cy.wait(3000);
   });
 });
 
