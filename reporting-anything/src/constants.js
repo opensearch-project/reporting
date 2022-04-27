a75b64ef0b742a5bd48e4e52a14bb1b3e4fc1d2b
David Cui <davidcui@amazon.com>
2022-04-27 11:39:45 -0700

    reporting-anything functional for URL or opensearch reports, added input validation
    Signed-off-by: David Cui <davidcui@amazon.com>

--- reporting-anything/src/constants.js
+++ reporting-anything/src/constants.js
@@ -6,14 +6,12 @@
 export const CHROMIUM_PATH = '../.chromium/headless_shell';
 
 export const REPORT_TYPE_URLS = {
-  DASHBOARD: 'dashboards#/view/',
-  VISUALIZATION: 'visualize#/edit/',
-  SAVED_SEARCH: 'discover#/view/',
+  DASHBOARD: 'dashboards#/view',
+  VISUALIZATION: 'visualize#/edit',
   NOTEBOOK: 'observability-dashboards#/notebooks'
 }
 
 export const REPORT_TYPE = {
-  SAVED_SEARCH: 'Saved search',
   DASHBOARD: 'Dashboard',
   VISUALIZATION: 'Visualization',
   NOTEBOOK: 'Notebook',
--- reporting-anything/src/download_helpers.js
+++ reporting-anything/src/download_helpers.js
@@ -5,11 +5,12 @@
 
 import puppeteer from 'puppeteer-core';
 import createDOMPurify from 'dompurify';
+import fs from 'fs';
 import { JSDOM } from 'jsdom';
-import { CHROMIUM_PATH, REPORT_TYPE_URLS, FORMAT, REPORT_TYPE } from './constants.js';
+import { CHROMIUM_PATH, REPORT_TYPE_URLS, FORMAT, REPORT_TYPE, SELECTOR } from './constants.js';
 
 
-export async function downloadVisualReport(url, type, object_id, format) {
+export async function downloadVisualReport(url, type, object_id, format, width, height) {
   const window = new JSDOM('').window;
   const DOMPurify = createDOMPurify(window);
 
@@ -44,16 +45,13 @@ export async function downloadVisualReport(url, type, object_id, format) {
       queryUrl = url;
     }
     else {
-      queryUrl = 'localhost:5601/app/';
+      queryUrl = 'http://localhost:5601/app/';
       // create URL from report source type and saved object ID
       if (type.toUpperCase() === 'DASHBOARD') {
         queryUrl += REPORT_TYPE_URLS.DASHBOARD;
       }
       else if (type.toUpperCase() === 'VISUALIZATION') {
         queryUrl += REPORT_TYPE_URLS.VISUALIZATION;
-      }
-      else if (type.toUpperCase() === 'SAVED SEARCH') {
-        queryUrl += REPORT_TYPE_URLS.SAVED_SEARCH;
       }
       else if (type.toUpperCase() === 'NOTEBOOK') {
         queryUrl += REPORT_TYPE_URLS.NOTEBOOK;
@@ -62,74 +60,95 @@ export async function downloadVisualReport(url, type, object_id, format) {
     }
     await page.goto(queryUrl, { waitUntil: 'networkidle0' });
     await page.setViewport({
-      width: 1680, // temp constants
-      height: 2560,
+      width: width,
+      height: height,
     });
 
+    let reportSource;
+    if (url !== undefined) {
+      reportSource = getReportSourceFromURL(url);
+    }
+    else {
+      reportSource = type;
+    }
 
-    const testGoogle = await browser.newPage();
-    await testGoogle.goto('https://www.google.com', {waitUntil: 'networkidle0'});
-    await testGoogle.evaluate(`() => {
-	document.title = 'New title';
-	
-    }`);
-    const reportSource = getReportSourceFromURL(url);
-    console.log('report source is', reportSource);
-    console.log('evaluated google');
-    // if its a report 
-    await page.evaluate(
-      (reportSource, REPORT_TYPE) => {
-        // remove buttons
-	console.log('in evaluate');
-        document
-          .querySelectorAll("[class^='euiButton']")
-          .forEach((e) => e.remove());
-        // remove top navBar
-	console.log('after first document statement');
-        document
-          .querySelectorAll("[class^='euiHeader']")
-          .forEach((e) => e.remove());
-	console.log('after second document statement');
-        // remove visualization editor
-        if (reportSource === REPORT_TYPE.VISUALIZATION) {
+    // if its an OpenSearch report, remove extra elements
+    if (reportSource !== 'Other') {
+      await page.evaluate(
+        (reportSource, REPORT_TYPE) => {
+          // remove buttons
           document
-            .querySelector('[data-test-subj="splitPanelResizer"]')
-            ?.remove();
-          document.querySelector('.visEditor__collapsibleSidebar')?.remove();
-        }
-	console.log('after third doc statement');
-        document.body.style.paddingTop = '0px';
-	console.log('at end of evaluate');
-      },
-      reportSource,
-      REPORT_TYPE
-    );
-    
+            .querySelectorAll("[class^='euiButton']")
+            .forEach((e) => e.remove());
+          // remove top navBar
+          document
+            .querySelectorAll("[class^='euiHeader']")
+            .forEach((e) => e.remove());
+          // remove visualization editor
+          if (reportSource === REPORT_TYPE.VISUALIZATION) {
+            document
+              .querySelector('[data-test-subj="splitPanelResizer"]')
+              ?.remove();
+            document.querySelector('.visEditor__collapsibleSidebar')?.remove();
+          }
+          document.body.style.paddingTop = '0px';
+        },
+        reportSource,
+        REPORT_TYPE
+      );      
+    }
+
       // force wait for any resize to load after the above DOM modification
       await page.waitFor(1000);
       switch (reportSource) {
         case 'Dashboard':
-          await page.waitForSelector(SELECTOR.dashboard, {
+          await page.waitForSelector(SELECTOR.DASHBOARD, {
             visible: true,
           });
           break;
         case 'Visualization':
-          await page.waitForSelector(SELECTOR.visualization, {
+          await page.waitForSelector(SELECTOR.VISUALIZATION, {
             visible: true,
           });
           break;
         case 'Notebook':
-          await page.waitForSelector(SELECTOR.notebook, {
+          await page.waitForSelector(SELECTOR.NOTEBOOK, {
             visible: true,
           });
           break;
         default:
-          throw Error(
-            `report source can only be one of [Dashboard, Visualization]`
-          );
+          break;
       }
 
       await waitForDynamicContent(page);
+
+      let buffer;
+      // create pdf or png accordingly
+      if (format === FORMAT.PDF) {
+        const scrollHeight = await page.evaluate(
+          /* istanbul ignore next */
+          () => document.documentElement.scrollHeight
+        );
+    
+        buffer = await page.pdf({
+          margin: undefined,
+          width: 1680,
+          height: scrollHeight + 'px',
+          printBackground: true,
+          pageRanges: '1',
+        });
+      } else if (format === FORMAT.PNG) {
+        buffer = await page.screenshot({
+          fullPage: true,
+        });
+      }
+    
+      const fileName = `reporting_anything.${format}`;
+      const curTime = new Date();
+      const timeCreated = curTime.valueOf();
+      await browser.close();
+      const data = { timeCreated, dataUrl: buffer.toString('base64'), fileName };
+      await readStreamToFile(data.dataUrl, fileName);
   } catch (e) {
     console.log('error is', e);
     process.exit(1);
@@ -161,32 +180,6 @@ const waitForDynamicContent = async (
     previousLength = currentLength;
     await page.waitFor(interval);
   }
-
-  let buffer;
-  // create pdf or png accordingly
-  if (format === FORMAT.PDF) {
-    const scrollHeight = await page.evaluate(
-      /* istanbul ignore next */
-      () => document.documentElement.scrollHeight
-    );
-
-    buffer = await page.pdf({
-      margin: undefined,
-      width: windowWidth,
-      height: scrollHeight + 'px',
-      printBackground: true,
-      pageRanges: '1',
-    });
-  } else if (format === FORMAT.PNG) {
-    buffer = await page.screenshot({
-      fullPage: true,
-    });
-  }
-
-  const curTime = new Date();
-  const timeCreated = curTime.valueOf();
-  await browser.close();
-  return { timeCreated, dataUrl: buffer.toString('base64'), fileName };
 };
 
 const getReportSourceFromURL = (url) => {
@@ -202,23 +195,20 @@ const getReportSourceFromURL = (url) => {
   else if (url.includes('notebooks')) {
     return 'Notebook';
   }
+  return 'Other';
 }
 
-function puppeteerEvaluate(reportSource) {
-  // remove buttons
-  document
-    .querySelectorAll("[class^='euiButton']")
-    .forEach((e) => e.remove());
-  // remove top navBar
-  document
-    .querySelectorAll("[class^='euiHeader']")
-    .forEach((e) => e.remove());
-  // remove visualization editor
-  if (reportSource === 'Visualization') {
-    document
-      .querySelector('[data-test-subj="splitPanelResizer"]')
-      ?.remove();
-    document.querySelector('.visEditor__collapsibleSidebar')?.remove();
-  }
-  document.body.style.paddingTop = '0px';
-}
+export const readStreamToFile = async (
+  stream,
+  fileName
+) => {
+  let base64Image = stream.split(';base64,').pop();
+  fs.writeFile(fileName, base64Image, {encoding: 'base64'}, function (err) {
+    console.log('Downloaded report');
+  })
+};
+
+export const getFileFormatPrefix = (fileFormat) => {
+  var fileFormatPrefix = 'data:' + fileFormat + ';base64,';
+  return fileFormatPrefix;
+};
