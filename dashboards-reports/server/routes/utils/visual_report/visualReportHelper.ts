4d62193c7cc884a00e93b82469206b587b28d1b4
Rupal Mahajan <maharup@amazon.com>
2022-10-04 20:13:40 +0000

    Change timeout
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>

--- dashboards-reports/server/routes/utils/visual_report/visualReportHelper.ts
+++ dashboards-reports/server/routes/utils/visual_report/visualReportHelper.ts
@@ -115,7 +115,7 @@ export const createVisualReport = async (
   });
 
   page.setDefaultNavigationTimeout(0);
-  page.setDefaultTimeout(180000); // use 180s timeout instead of default 30s
+  page.setDefaultTimeout(300000); // use 300s timeout instead of default 30s
   // Set extra headers that are needed
   if (!_.isEmpty(extraHeaders)) {
     await page.setExtraHTTPHeaders(extraHeaders);
