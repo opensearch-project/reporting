a607c8c838055b1fb0aba2b7e59a5359c0afc072
Rupal <maharup@amazon.com>
2022-10-05 13:06:10 -0700

    Upgrade puppeteer (#483)
    * Upgrade puppeteer & change report timeout
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Change default timeout
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>

--- dashboards-reports/server/routes/utils/visual_report/visualReportHelper.ts
+++ dashboards-reports/server/routes/utils/visual_report/visualReportHelper.ts
@@ -74,7 +74,6 @@ export const createVisualReport = async (
       '--disable-setuid-sandbox',
       '--disable-gpu',
       '--no-zygote',
-      '--single-process',
       '--font-render-hinting=none',
     ],
     executablePath: CHROMIUM_PATH,
@@ -86,7 +85,7 @@ export const createVisualReport = async (
   });
   const page = await browser.newPage();
   page.setDefaultNavigationTimeout(0);
-  page.setDefaultTimeout(100000); // use 100s timeout instead of default 30s
+  page.setDefaultTimeout(300000); // use 300s timeout instead of default 30s
   // Set extra headers that are needed
   if (!_.isEmpty(extraHeaders)) {
     await page.setExtraHTTPHeaders(extraHeaders);
@@ -136,7 +135,7 @@ export const createVisualReport = async (
   );
 
   // force wait for any resize to load after the above DOM modification
-  await page.waitFor(1000);
+  await new Promise(resolve => setTimeout(resolve, 1000));
   // crop content
   switch (reportSource) {
     case REPORT_TYPE.dashboard:
@@ -271,6 +270,6 @@ const waitForDynamicContent = async (
     }
 
     previousLength = currentLength;
-    await page.waitFor(interval);
+    await new Promise(resolve => setTimeout(resolve, interval));
   }
 };
