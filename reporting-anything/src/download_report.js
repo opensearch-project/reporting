070d53f745cf90203178521bb4d5d8374bb55ee9
Rupal Mahajan <maharup@amazon.com>
2022-10-24 16:56:45 +0000

    Reorganize command arguments
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>

--- reporting-anything/src/download_report.js
+++ reporting-anything/src/download_report.js
@@ -14,67 +14,28 @@ dotenv.config();
 
 // url OR report source type and ID
 const parser = new ArgumentParser({
-  description: 'Argument parser for Reporting Anything'
+  description: 'Argument parser for Reporting CLI'
 });
 
-parser.add_argument('-u', '--url', {help: 'url of report'});
-parser.add_argument('-f', '--format', {help: 'File format of the report'});
-parser.add_argument('-w', '--width', { help: 'Window width of the report'});
-parser.add_argument('-t', '--height', {help: 'Window height of the report'});
-parser.add_argument('-n', '--filename', {help: 'File name of the report'});
-parser.add_argument('-a', '--auth', {help: 'authentication type of the report; options are bas'});
-parser.add_argument('-c', '--credentials', {help: 'login credentials in the format of username:password'});
-parser.add_argument('-s', '--sender', {help: 'email address of sender'});
-parser.add_argument('-r', '--recipient', {help: 'email address of recipient'});
+parser.add_argument('--url', {help: 'url of report'});
+parser.add_argument('--format', {help: 'File format of the report'});
+parser.add_argument('--width', { help: 'Window width of the report'});
+parser.add_argument('--height', {help: 'Window height of the report'});
+parser.add_argument('--filename', {help: 'File name of the report'});
+parser.add_argument('--auth', {help: 'authentication type of the report; options are bas'});
+parser.add_argument('--credentials', {help: 'login credentials in the format of username:password'});
+parser.add_argument('--from', {help: 'email address of the sender'});
+parser.add_argument('--to', {help: 'email address of the recipient'});
 
 const parsed_args = parser.parse_args();
 
+// Get URL for the report.
 if (parsed_args.url === undefined) {
   console.log('Please specify a URL');
   exit(1);
 }
 
-if (parsed_args.format !== undefined && 
-    parsed_args.format.toUpperCase() !== 'PDF' && 
-    parsed_args.format.toUpperCase() !== 'PNG') {
-  console.log('Please specify a valid file format: one of PDF or PNG');
-  exit(1);
-}
-
-let format;
-
-// Set default format to PDF if not specified.
-if (parsed_args.format === undefined) {
-  format = 'pdf';
-} else {
-  format = parsed_args.format;
-}
-
-
-// Set default width and height if not specified.
-let width, height;
-if (parsed_args.width === undefined) {
-  width = 1680;
-}
-else {
-  width = Number(parsed_args.width)
-}
-if (parsed_args.height === undefined) {
-  height = 600;
-}
-else {
-  height = Number(parsed_args.height)
-}
-
-// Set default filename is not specified.
-let filename;
-if (parsed_args.filename !== undefined) {
-  filename = parsed_args.filename;
-}
-else {
-  filename = 'reporting_anything';
-}
-
+// Get authentication type.
 let authType;
 if (parsed_args.auth !== undefined) {
   authType = parsed_args.auth;
@@ -98,4 +59,49 @@ if(authType != undefined && authType != 'none' && username == undefined && passw
   exit(1);
 }
 
-await downloadVisualReport(parsed_args.url, format, width, height, filename, authType, username, password, parsed_args.sender, parsed_args.recipient);
+// Validate report format.
+if (parsed_args.format !== undefined && 
+    parsed_args.format.toUpperCase() !== 'PDF' && 
+    parsed_args.format.toUpperCase() !== 'PNG') {
+  console.log('Please specify a valid file format: one of PDF or PNG');
+  exit(1);
+}
+
+// Set default format to PDF if not specified.
+let format;
+if (parsed_args.format === undefined) {
+  format = 'pdf';
+} else {
+  format = parsed_args.format;
+}
+
+// Set default width and height if not specified.
+let width, height;
+if (parsed_args.width === undefined) {
+  width = 1680;
+}
+else {
+  width = Number(parsed_args.width)
+}
+if (parsed_args.height === undefined) {
+  height = 600;
+}
+else {
+  height = Number(parsed_args.height)
+}
+
+// Set default filename is not specified.
+let filename = process.env.FILENAME;
+if (filename === undefined || filename.length <=0 && parsed_args.filename !== undefined) {
+  filename = parsed_args.filename;
+}
+else {
+  filename = 'reporting_anything';
+}
+
+
+// Set email addresse if specified
+let sender = process.env.FROM || parsed_args.from
+let recipient = process.env.TO || parsed_args.to
+
+await downloadVisualReport(parsed_args.url, format, width, height, filename, authType, username, password, sender, recipient);
