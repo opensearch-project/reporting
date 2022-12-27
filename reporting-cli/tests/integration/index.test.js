887e4b8104cd4039440f0741e4c92f93d8aab959
Rupal Mahajan <maharup@amazon.com>
2022-12-27 04:06:17 +0000

    Try integration tests run
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>

--- reporting-cli/tests/integration/index.test.js
+++ reporting-cli/tests/integration/index.test.js
@@ -83,22 +83,21 @@ describe('download report options', () => {
     fs.unlinkSync(expectedFile);
   }, 150000);
 
-  test('dashboard report with custom tenant', async () => {
-    let result = await cli(['-u', 'http://localhost:5601/app/dashboards#/view/7adfa750-4c81-11e8-b3d7-01146121b73d', '-a', 'basic', '-c', 'admin:admin',
-      '-n', 'customtenantdashboard', '-t', 'admin_tenant'], '.');
-    expect(result.code).toBe(0);
-    const expectedFile = './customtenantdashboard.pdf';
-    const stats = fs.statSync(expectedFile);
-    expect(stats.size >= 0).toBeTruthy();
-    fs.unlinkSync(expectedFile);
-  }, 150000);
-
-
   test('dashboard report with global tenant', async () => {
     let result = await cli(['-u', 'http://localhost:5601/app/dashboards#/view/edf84fe0-e1a0-11e7-b6d5-4dc382ef7f5b', '-a', 'basic', '-c', 'admin:admin',
       '-n', 'globaltenantdashboard', '-t', 'global'], '.');
     expect(result.code).toBe(0);
     const expectedFile = './globaltenantdashboard.pdf';
+    const stats = fs.statSync(expectedFile);
+    expect(stats.size >= 0).toBeTruthy();
+    fs.unlinkSync(expectedFile);
+  }, 150000);
+
+  test('dashboard report with custom tenant', async () => {
+    let result = await cli(['-u', 'http://localhost:5601/app/dashboards#/view/7adfa750-4c81-11e8-b3d7-01146121b73d', '-a', 'basic', '-c', 'admin:admin',
+      '-n', 'customtenantdashboard', '-t', 'admin_tenant'], '.');
+    expect(result.code).toBe(0);
+    const expectedFile = './customtenantdashboard.pdf';
     const stats = fs.statSync(expectedFile);
     expect(stats.size >= 0).toBeTruthy();
     fs.unlinkSync(expectedFile);
