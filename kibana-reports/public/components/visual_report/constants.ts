3ab1bc6ce931e1c2c2386dad7e90f7ccb097bf42
Shenoy Pratik <sgguruda@amazon.com>
2022-12-29 11:42:21 -0800

    replace header name
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>

--- kibana-reports/public/components/visual_report/constants.ts
+++ kibana-reports/public/components/visual_report/constants.ts
@@ -23,7 +23,7 @@ export enum SELECTOR {
   notebook = '.euiPageBody',
 }
 
-export const DEFAULT_REPORT_HEADER = '<h1>OpenSearch Dashboards Reports</h1>';
+export const DEFAULT_REPORT_HEADER = '<h1>Open Distro Kibana Reports</h1>';
 
 export const converter = new Showdown.Converter({
   tables: true,
