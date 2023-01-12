48fb0a30711b2498639807eeac3c9c3c97e4aac6
Rupal Mahajan <maharup@amazon.com>
2023-01-12 22:39:55 +0000

    [1.3] Remove front end code (#634)
    * Remove frontend code
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Move one level up and update CI
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Fix workflow
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>

--- dashboards-reports/public/components/main/index.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export { Main } from './main';
-export { ReportDetails } from './report_details/report_details';
